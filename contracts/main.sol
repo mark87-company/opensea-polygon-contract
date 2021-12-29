ea7e9cc55987fcb8dd6f5b18645511de1f593a77
Asher Hwang <dev@hsh.kr>
2021-12-29 09:21:49 +0900

    Fetch submodule

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
+import "../MAS_contracts/polygon/bank.sol";
 import "../MAS_contracts/polygon/ERC1155_modifier.sol";
 import "../MAS_contracts/polygon/factory.sol";
 import "../MAS_contracts/polygon/MAS_Sales.sol";
-import "../MAS_contracts/polygon/MAS_WhiteLists.sol";
 import "../MAS_contracts/polygon/MasRoyalty.sol";
+import "../MAS_contracts/polygon/whiteLists.sol";
