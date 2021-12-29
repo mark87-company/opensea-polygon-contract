8b10e6b1257b52fac13aabbac356ea9894b5a24c
Asher Hwang <dev@hsh.kr>
2021-12-29 14:57:51 +0900

    v0.2.3

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { default as MAS } from "./deployed-contracts/MAS";
+export { default as MASBank } from "./deployed-contracts/MASBank";
+export { default as MASRoyalty } from "./deployed-contracts/MASRoyalty";
+export { default as MASWhiteLists } from "./deployed-contracts/MASWhiteLists";
+export { default as NFTSales } from "./deployed-contracts/NFTSales";
